d6a57a74382b24b3fa831c150be4c662418e53a6
Vlado Situm <61758568+vsitum@users.noreply.github.com>
2021-03-17 21:26:49 -0700

    comm

--- states.py
+++ states.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 hostname="127.0.0.1"
 uname="root"
 pwd=""
-dbname="zip_codes"
+dbname="zipcodes"
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
             .format(host=hostname, db=dbname, user=uname, pw=pwd))
@@ -14,6 +14,6 @@ tables = pandas.read_csv(r"D:\DB-3\RTC\zip_code_database.csv")
 
 
 connection=engine.connect()
-tables.to_sql('zip_codes',con = engine, if_exists = 'replace')
+tables.to_sql('zipcodes',con = engine, if_exists = 'replace')
 connection.close()
 print(tables)
